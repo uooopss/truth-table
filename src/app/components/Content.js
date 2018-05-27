05914a5e7625b7b4e050e3c23a56408fd3aaa99b
uooopss <skipor.ed@gmail.com>
2018-05-27 22:40:01 +0300

    add tb file and fixed styles

--- src/app/components/Content.js
+++ src/app/components/Content.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Select from "react-select";
+import { Tabs, Tab, Alert } from "react-bootstrap";
 import update from "react-addons-update";
 import { Table } from 'reactstrap';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 import data from "../../../output.json"
 
@@ -56,28 +56,16 @@ export class Content extends React.Component {
                 }
             ],
             arrayDNF: [],
-            arrVHDL: [],
             clearable: true,
             selectOperand: '',
             selectBit: '',
             show: false
         };
         this.convert = this.convert.bind(this);
-        this.outputVHDL = this.outputVHDL.bind(this);
     }
 
     handleSelectOperand = (e, name) => {
         console.log("name", e, name)
-        // if (selectOperand) {
-        //     for (var i = 0; i < selectOperand.value; i++) {
-        //         const obj = {
-        //             index: i,
-        //             groupLetter: String.fromCharCode(65 + i),
-        //             groupMultiplier: ''
-        //         }
-        //         arr.push(obj);
-        //     }
-        // }
         if (name === 'selOperand') {
             this.setState({
                 selectOperand: e,
@@ -89,15 +77,6 @@ export class Content extends React.Component {
         }
 
     }
-
-    // handleChange = (e, i) => {
-    //     console.log("input index", i);
-    //     this.setState({
-    //         arrayGroups: update(this.state.arrayGroups, {
-    //             [i]: { 'groupMultiplier': { $set: e.target.value } }
-    //         })
-    //     }, () => { console.log("update", this.state.arrayGroups) })
-    // }
 
     convert(row, num) {
         const arr = [...row];
@@ -189,25 +168,10 @@ export class Content extends React.Component {
                 }
             })
         })
-        this.outputVHDL(arrDNF);
         this.setState({
             arrayDNF: arrDNF,
             show: true
         })
-    }
-
-    outputVHDL(arr) {
-        const mul = this.state.selectBit.value * this.state.selectOperand.value;
-        var ar = [];
-        for (var p = 0; p < mul; p++) {
-            const o = {
-                index: p
-            }
-            ar.push(o);
-            }
-        this.setState({
-            arrVHDL: ar
-        }, () => {console.log("arrrrr", this.state.arrVHDL)})
     }
 
     render() {
@@ -230,9 +194,18 @@ export class Content extends React.Component {
         return (
             <main className="content">
                 <div className="container-fluid">
+                    {validNumbers > 16 &&
+                        (<div className="row">
+                            <div className="col-12 alert-pad">
+                                <Alert bsStyle="warning" className="d-flex justify-content-center">
+                                    <strong>Warning!</strong> 
+                                    <p className="alert-p-mar">The sum of all bits must not exceed 16</p>
+                                </Alert>;
+                            </div>
+                        </div>)
+                    }
                     <div className="row">
-                        <div className="col-4 content-menu d-flex justify-content-center align-items-center">
-                            {/* <h1>menu selects</h1> */}
+                        <div className="col-4 content-menu ">
                             <div className="col-11 mt-4 mb-4 content-form">
                                 <form>
                                     <div className="form-group row d-flex align-items-center">
@@ -260,101 +233,115 @@ export class Content extends React.Component {
                                     {validNumbers <= 16 && (
                                         <div className="form-group row form-button">
                                             <div className="col-12 d-flex justify-content-center">
-                                                <button type="button" className="btn btn-warning" onClick={() => this.build(validNumbers)}>BUILD</button>
+                                                <button type="button" className="btn btn-warning but" onClick={() => this.build(validNumbers)}>BUILD</button>
+                                                <button type="button" className="btn btn-warning but" disabled={show ? true : false} onClick={() => this.output()}>OUTPUT</button>
                                             </div>
                                         </div>
                                     )}
                                 </form>
                             </div>
-                            {/* <div>
-                                <button type="button" className="btn btn-warning" onClick={() => this.output()}>OUTPUT</button>
-                            </div> */}
                         </div>
                         <div className="col-8 content-main d-flex justify-content-center align-items-center">
-                            <div className="col-9">
+                            <div className="col-6">
                                 {!show ?
-                                    (<Table bordered>
-                                        <thead>
-                                            <tr>
-                                                {array1[0].A.map((o, i) => (<th key={i}>{o}</th>))}
-                                                {array1[0].R.map((o, i) => (<th key={i}>{o}</th>))}
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {array[0].A.map((o, i) => (
-                                                <tr key={i}>
-                                                    {o.value.a.map((ob, ii) => (
-                                                        <td key={ii}>
-                                                            {ob}
-                                                        </td>))}
-                                                    {o.value.r.map((ob, ii) => (
-                                                        <td key={ii}>
-                                                            {ob}
-                                                        </td>))}
-                                                </tr>))}
-                                            {/* {array[0].R.map((o,i) => (
-                                        <tr key={i}>
-                                            {o.value.map((ob,ii) => (
-                                                <td key={ii}>
-                                                    {+ob} 
-                                                </td>))}
-                                        </tr>))} */}
-                                        </tbody>
-                                    </Table>) :
-                                    // (this.state.arrVhdl.props.children.map((obj, i) => (
-                                    //     (i !== 11 ? 
-                                    //         <div>{obj.props.children.map((o, ind) => (<p key={ind}> {o}</p>))}</div> :
-                                    //         <div> {obj.map((oo, ii) => (<div key={ii}>{oo.props.children.map((ooo, iii) => 
-                                    //             (<p key={iii}>{ooo}</p>)
-                                    //         )}</div>))}</div>
-                                    //     )
-                                        
-                                    // )
-                                        
-                                    // ))
-                                    (this.state.arrayDNF.map((o, i) => (<div key={i}>
-                                        <p>entity f{o.index} is</p>
-                                        <p>port(</p>
-                                        <p>x : in std_logic_vector([{validNumbers}-1] downto 0);</p>
-                                        <p>f:out std_logic</p>
-                                        <p>);</p>
-                                        <p>end entity;</p>
-                                        <p>architecture arch of f{o.index} is</p>
-                                        <p>signal con : std_logic_vector([{arrayDNF.length}-1] downto 0):= (others => '0');</p>
-                                        <p>signal result: std_logic := '0';</p>
-                                        <p>type in_data is array(0 to [{arrayDNF.length}-1]) of std_logic_vector([{validNumbers}-1] downto 0);</p>
-                                        <p>signal table: in_data :=(</p>
-                                        {o.name.map((obj, ii) => (
-                                            <p key={ii}>"{obj}"</p>
-                                        ))}
-                                        <p>);</p>
-                                        <p>begin</p>
-                                        <p>process(x)</p>
-                                        <p>variable word: std_logic_vector([{validNumbers}-1] downto 0);</p>
-                                        <p>variable i, j, k: integer := 0;</p>
-                                        <p>begin</p>
-                                        <p>for i in 0 to [{arrayDNF.length}-1] loop</p>
-                                        <p>word := table(i);</p>
-                                        <p>for j in 0 to [{validNumbers}-1] loop</p>
-                                        <p>if word(j) = '1' then</p>
-                                        <p>{"con(i) <= con(i) and x(j);"}</p>
-                                        <p>end if;</p>
-                                        <p>end loop;</p>
-                                        <p>end loop;</p>
-                        
-                                        <p>for k in 0 to [{arrayDNF.length}-1] loop</p>
-                                        <p>{"result <= result or con(k);"}</p>
-                                        <p>end loop;</p>
-                                        <p>end process;</p>
-                                        <p>end arch;</p>
-                                    </div>)))
+                                         (<Table bordered> 
+                                            <thead>
+                                                <tr>
+                                                    {array1[0].A.map((o, i) => (<th key={i}>{o}</th>))}
+                                                    {array1[0].R.map((o, i) => (<th key={i}>{o}</th>))}
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {array[0].A.map((o, i) => (
+                                                    <tr key={i}>
+                                                        {o.value.a.map((ob, ii) => (
+                                                            <td key={ii}>
+                                                                {ob}
+                                                            </td>))}
+                                                        {o.value.r.map((ob, ii) => (
+                                                            <td key={ii}>
+                                                                {ob}
+                                                            </td>))}
+                                                    </tr>))}
+                                            </tbody>
+                                        </Table>) :
+                                    (
+                                        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example" className="tabs-margin">
+                                            <Tab eventKey={1} title={"tb"}>
+                                                <div className="borFiles">
+                                                    <p>library ieee;</p>
+                                                    <p>use ieee.std_logic_1164.all;</p>
+                                                    <p>entity tb is</p>
+                                                    <p>end entity tb;</p>
+                                                    <p>architecture TEST of tb is</p>
+                                                    <p>signal x: std_logic_vector([{validNumbers} -1] downto 0);</p>
+                                                    <p>signal output: std_logic_vector (0 to [{validNumbers}-1]);</p>
+                                                    {arrayDNF.map((o, i) => (
+                                                        <div key={i}>
+                                                            <p>component f{i}</p>
+                                                            <p>port (</p>
+                                                            <p>x : in std_logic_vector([{validNumbers}-1] downto 0);</p>
+                                                            <p>f:out std_logic</p>
+                                                            <p>);</p>
+                                                            <p>end component;</p>
+                                                        </div>
+                                                    ))}
+                                                    <p>begin</p>
+                                                    <p>{"x <= x + 1;"}</p>
+                                                    {arrayDNF.map((o, i) => (
+                                                        <div key={i}>
+                                                            <p>inst_f{i}: f{i}</p>
+                                                            <p>port map(x => x, f => output({i}));</p>
+                                                        </div>
+                                                    ))}
+                                                    <p>end architecture TEST;</p>
+                                                </div>
+                                            </Tab>
+                                            {this.state.arrayDNF.map((o, i) => (
+
+                                                <Tab key={i} eventKey={i + 2} title={"f" + i}>
+                                                    <div className="borFiles">
+                                                        <p>entity f{i} is</p>
+                                                        <p>port(</p>
+                                                        <p>x : in std_logic_vector([{validNumbers}-1] downto 0);</p>
+                                                        <p>f:out std_logic</p>
+                                                        <p>);</p>
+                                                        <p>end entity;</p>
+                                                        <p>architecture arch of f{i} is</p>
+                                                        <p>signal con : std_logic_vector([{arrayDNF.length}-1] downto 0):= (others => '0');</p>
+                                                        <p>signal result: std_logic := '0';</p>
+                                                        <p>type in_data is array(0 to [{arrayDNF.length}-1]) of std_logic_vector([{validNumbers}-1] downto 0);</p>
+                                                        <p>signal table: in_data :=(</p>
+                                                        {o.name.map((obj, ii) => (
+                                                            <p key={ii}>"{obj}"</p>
+                                                        ))}
+                                                        <p>);</p>
+                                                        <p>begin</p>
+                                                        <p>process(x)</p>
+                                                        <p>variable word: std_logic_vector([{validNumbers}-1] downto 0);</p>
+                                                        <p>variable i, j, k: integer := 0;</p>
+                                                        <p>begin</p>
+                                                        <p>for i in 0 to [{arrayDNF.length}-1] loop</p>
+                                                        <p>word := table(i);</p>
+                                                        <p>for j in 0 to [{validNumbers}-1] loop</p>
+                                                        <p>if word(j) = '1' then</p>
+                                                        <p>{"con(i) <= con(i) and x(j);"}</p>
+                                                        <p>end if;</p>
+                                                        <p>end loop;</p>
+                                                        <p>end loop;</p>
+
+                                                        <p>for k in 0 to [{arrayDNF.length}-1] loop</p>
+                                                        <p>{"result <= result or con(k);"}</p>
+                                                        <p>end loop;</p>
+                                                        <p>end process;</p>
+                                                        <p>end arch;</p>
+                                                    </div>
+                                                </Tab>))}
+                                        </Tabs>)
                                 }
                             </div>
-                            <div className="col-3">
-                                <button type="button" className="btn btn-warning" onClick={() => this.output()}>OUTPUT</button>
+                            <div className="col-6">
                             </div>
-
-
                         </div>
                     </div>
                 </div>
