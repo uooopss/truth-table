e1487293aab78521f14bb46d7e8a5241b5a6780f
uooopss <skipor.ed@gmail.com>
2018-05-27 17:00:20 +0300

    add main vhdl

--- src/app/components/Content.js
+++ src/app/components/Content.js
@@ -2,6 +2,7 @@ import React from "react";
 import Select from "react-select";
 import update from "react-addons-update";
 import { Table } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 import data from "../../../output.json"
 
@@ -55,11 +56,14 @@ export class Content extends React.Component {
                 }
             ],
             arrayDNF: [],
+            arrVHDL: [],
             clearable: true,
             selectOperand: '',
-            selectBit: ''
+            selectBit: '',
+            show: false
         };
         this.convert = this.convert.bind(this);
+        this.outputVHDL = this.outputVHDL.bind(this);
     }
 
     handleSelectOperand = (e, name) => {
@@ -83,7 +87,7 @@ export class Content extends React.Component {
                 selectBit: e,
             }, () => { console.log("selectBit", this.state.selectBit) })
         }
-       
+
     }
 
     // handleChange = (e, i) => {
@@ -107,7 +111,7 @@ export class Content extends React.Component {
             arr.splice(0, this.state.selectBit.value);
         }
         const number = String((n >>> 0).toString(2)).split("");
-        while(number.length !== num) {
+        while (number.length !== num) {
             number.unshift('0');
         }
         return number;
@@ -125,18 +129,18 @@ export class Content extends React.Component {
                 arr1.push(letterA + j);
             }
         }
-        
+
         for (var i = 0; i < Math.pow(2, num); i++) {
             const row = [];
             var str = '';
             // 17
-            for (var j = (num - 1) ; j >= 0 ; j--) {
-                const a = (i / Math.pow(2,j)) % 2;
-                row[num-1 -j] = Math.floor(a);
+            for (var j = (num - 1); j >= 0; j--) {
+                const a = (i / Math.pow(2, j)) % 2;
+                row[num - 1 - j] = Math.floor(a);
                 if (i === num - 1) {
-                    arr2.push(letterR + (num -1 -j));
+                    arr2.push(letterR + (num - 1 - j));
                 }
-                
+
                 str += Math.floor(a);
             }
             const o = {
@@ -147,46 +151,63 @@ export class Content extends React.Component {
                 }
             }
             arr.push(o);
-            
+
         }
         this.setState({
             array: update(this.state.array, {
                 [0]: {
                     'A': { $set: arr }
                 }
-            }), 
+            }),
             array1: update(this.state.array1, {
                 [0]: {
                     'A': { $set: arr1 },
-                    'R': {$set: arr2}
+                    'R': { $set: arr2 }
                 }
-            })
-        }, () => {console.log("arr2", this.state.array)})
+            }),
+            show: false
+        }, () => { console.log("arr2", this.state.array) })
     }
 
     output() {
         console.log("output", data);
         console.log("shift", data.output.shift())
         var arrDNF = [];
-            data.output.map((obj) => {
-                const ar = Array.from(obj.R);
-                ar.forEach((n, i) => {
-                    const idx = arrDNF.map(ii => ii.index).indexOf(i);
-                    if(+n === 1 && (arrDNF.length === 0 || idx === -1)) {
-                        const p = {
-                            index: i,
-                            name: [obj.A]
-                        }
-                        arrDNF.push(p);
-                    } 
-                    else if (+n === 1 && idx !== -1) {
-                        arrDNF[idx].name.push(obj.A)
+        data.output.map((obj) => {
+            const ar = Array.from(obj.R);
+            ar.forEach((n, i) => {
+                const idx = arrDNF.map(ii => ii.index).indexOf(i);
+                if (+n === 1 && (arrDNF.length === 0 || idx === -1)) {
+                    const p = {
+                        index: i,
+                        name: [obj.A]
                     }
-                })
+                    arrDNF.push(p);
+                }
+                else if (+n === 1 && idx !== -1) {
+                    arrDNF[idx].name.push(obj.A)
+                }
             })
-            this.setState({
-                arrayDNF: arrDNF
-            }, () => {console.log("arrayDNF", this.state.arrayDNF)})
+        })
+        this.outputVHDL(arrDNF);
+        this.setState({
+            arrayDNF: arrDNF,
+            show: true
+        })
+    }
+
+    outputVHDL(arr) {
+        const mul = this.state.selectBit.value * this.state.selectOperand.value;
+        var ar = [];
+        for (var p = 0; p < mul; p++) {
+            const o = {
+                index: p
+            }
+            ar.push(o);
+            }
+        this.setState({
+            arrVHDL: ar
+        }, () => {console.log("arrrrr", this.state.arrVHDL)})
     }
 
     render() {
@@ -197,42 +218,14 @@ export class Content extends React.Component {
             arrSelectOperands,
             arrayOperands,
             array,
-            array1
+            array1,
+            arrayDNF,
+            show
         } = this.state;
         const valueOperand = selectOperand && selectOperand.value;
         const valueBit = selectBit && selectBit.value;
         const validNumbers = ((selectOperand !== null && selectBit !== null) && selectOperand.value * selectBit.value);
         const selectOper = "selOperand";
-
-        function Verilog() {
-
-            return (<div>
-                <textarea>
-                    module jdoodle;
-                        reg[10*10:0] string;
-                        reg[3:0] mem;
-                        initial begin
-                             string = "D:\espresso\output.txt";
-                             i = 0;
-                             repeat (16) begin
-                                 $display ("Current value of i is %d", i);
-                                 i = i + 1;
-                             end
-                             data_file = $fopen("D:\\espresso\\ver.txt");
-                               if (data_file == 0) begin
-                               end
-                        end
-                         always @(*) begin
-                           scan_file = $fscanf(data_file, "%d", captured_data); 
-                           if (!$feof(data_file)) begin
-                             //use captured_data as you would any other wire or reg value;
-                             $display ("Welcome to JDoodle!!! %s");
-                           end
-                         end
-                    endmodule
-                </textarea>
-            </div>)
-        }
 
         return (
             <main className="content">
@@ -279,38 +272,88 @@ export class Content extends React.Component {
                         </div>
                         <div className="col-8 content-main d-flex justify-content-center align-items-center">
                             <div className="col-9">
-                                <Table bordered>
-                                    <thead>
-                                        <tr>
-                                            {array1[0].A.map((o,i) => (<th key={i}>{o}</th>))}
-                                            {array1[0].R.map((o,i) => (<th key={i}>{o}</th>))}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {array[0].A.map((o,i) => (
-                                            <tr key={i}>
-                                                {o.value.a.map((ob,ii) => (
-                                                    <td key={ii}>
-                                                        {ob}
-                                                    </td>))}
-                                                {o.value.r.map((ob,ii) => (
-                                                <td key={ii}>
-                                                    {ob}
-                                                </td>))}
-                                            </tr>))}
-                                        {/* {array[0].R.map((o,i) => (
+                                {!show ?
+                                    (<Table bordered>
+                                        <thead>
+                                            <tr>
+                                                {array1[0].A.map((o, i) => (<th key={i}>{o}</th>))}
+                                                {array1[0].R.map((o, i) => (<th key={i}>{o}</th>))}
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {array[0].A.map((o, i) => (
+                                                <tr key={i}>
+                                                    {o.value.a.map((ob, ii) => (
+                                                        <td key={ii}>
+                                                            {ob}
+                                                        </td>))}
+                                                    {o.value.r.map((ob, ii) => (
+                                                        <td key={ii}>
+                                                            {ob}
+                                                        </td>))}
+                                                </tr>))}
+                                            {/* {array[0].R.map((o,i) => (
                                         <tr key={i}>
                                             {o.value.map((ob,ii) => (
                                                 <td key={ii}>
                                                     {+ob} 
                                                 </td>))}
                                         </tr>))} */}
-                                    </tbody>
-                                </Table>
+                                        </tbody>
+                                    </Table>) :
+                                    // (this.state.arrVhdl.props.children.map((obj, i) => (
+                                    //     (i !== 11 ? 
+                                    //         <div>{obj.props.children.map((o, ind) => (<p key={ind}> {o}</p>))}</div> :
+                                    //         <div> {obj.map((oo, ii) => (<div key={ii}>{oo.props.children.map((ooo, iii) => 
+                                    //             (<p key={iii}>{ooo}</p>)
+                                    //         )}</div>))}</div>
+                                    //     )
+                                        
+                                    // )
+                                        
+                                    // ))
+                                    (this.state.arrayDNF.map((o, i) => (<div key={i}>
+                                        <p>entity f{o.index} is</p>
+                                        <p>port(</p>
+                                        <p>x : in std_logic_vector([{validNumbers}-1] downto 0);</p>
+                                        <p>f:out std_logic</p>
+                                        <p>);</p>
+                                        <p>end entity;</p>
+                                        <p>architecture arch of f{o.index} is</p>
+                                        <p>signal con : std_logic_vector([{arrayDNF.length}-1] downto 0):= (others => '0');</p>
+                                        <p>signal result: std_logic := '0';</p>
+                                        <p>type in_data is array(0 to [{arrayDNF.length}-1]) of std_logic_vector([{validNumbers}-1] downto 0);</p>
+                                        <p>signal table: in_data :=(</p>
+                                        {o.name.map((obj, ii) => (
+                                            <p key={ii}>"{obj}"</p>
+                                        ))}
+                                        <p>);</p>
+                                        <p>begin</p>
+                                        <p>process(x)</p>
+                                        <p>variable word: std_logic_vector([{validNumbers}-1] downto 0);</p>
+                                        <p>variable i, j, k: integer := 0;</p>
+                                        <p>begin</p>
+                                        <p>for i in 0 to [{arrayDNF.length}-1] loop</p>
+                                        <p>word := table(i);</p>
+                                        <p>for j in 0 to [{validNumbers}-1] loop</p>
+                                        <p>if word(j) = '1' then</p>
+                                        <p>{"con(i) <= con(i) and x(j);"}</p>
+                                        <p>end if;</p>
+                                        <p>end loop;</p>
+                                        <p>end loop;</p>
+                        
+                                        <p>for k in 0 to [{arrayDNF.length}-1] loop</p>
+                                        <p>{"result <= result or con(k);"}</p>
+                                        <p>end loop;</p>
+                                        <p>end process;</p>
+                                        <p>end arch;</p>
+                                    </div>)))
+                                }
                             </div>
                             <div className="col-3">
                                 <button type="button" className="btn btn-warning" onClick={() => this.output()}>OUTPUT</button>
                             </div>
+
 
                         </div>
                     </div>
